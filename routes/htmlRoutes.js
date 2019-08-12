1cc755ff47b85dcee0f040a5fc67be67fd5f5bda
Xoelos <48268783+Xoelos@users.noreply.github.com>
2019-08-12 15:33:11 -0600

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,6 @@ var path = require('path');
 module.exports = function(app) {
     // viewed at http://localhost:8080
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname + '/index.html'));
+        res.sendFile(path.join(__dirname + '../../index.html'));
     });
 };
