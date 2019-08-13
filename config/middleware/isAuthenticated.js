0fcc148df8771da72229e9b4e93ea29a1ee62e83
Xoelos <48268783+Xoelos@users.noreply.github.com>
2019-08-13 13:57:47 -0600

    Update isAuthenticated.js

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -2,13 +2,9 @@
 module.exports = function(req, res, next) {
   // If the user is logged in, continue with the request to the restricted route
   if (req.user) {
-    console.log("REQ.USER is: ");
-    console.table(req.user);
+
     return next();
   } else {
-    console.log(
-      `Invalid Authentication from address: ${req.connection.remoteAddress}`
-    );
     // If the user isn't logged in, redirect them to the login page
     return res.redirect("/home");
   }
