a6078bd1e69feaa0d78ef10577ed33053b7427a6
Xoelos <48268783+Xoelos@users.noreply.github.com>
2019-08-22 08:50:03 -0600

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    article: require('./Article')
+  user: require("./User"),
+  userData: require("./userData")
 };
--- server.js
+++ server.js
@@ -1,47 +1,67 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+require('dotenv').config();
 
-var db = require("./models");
+const express = require('express');
+const session = require('express-session');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const uristring = process.env.MONGODB_URI || 'mongodb://localhost/project3';
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
+const cors = require('cors');
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://project3du.s3-website.us-east-2.amazonaws.com'
+    ],
+    credentials: true,
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
   })
 );
-app.set("view engine", "handlebars");
 
+// Requiring passport as we've configured it
+const passport = require('./config/passport');
+
+// Creating express app and configuring middleware needed for authentication
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === 'test') {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Connect to the Mongo DB
+mongoose.connect(uristring, function(err, res) {
+  if (err) {
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log('Succeeded connected to: ' + uristring);
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log('App running on port ' + PORT + '!');
 });
 
 module.exports = app;
